350f66b821609c1dd4ace41c9899b412dd091142
Bilgehan Zeki ÖZAYTAÇ <zekozaytac@hotmail.com.tr>
2021-04-27 23:31:53 +0300

    Projeye Entity Framework Eklendi

---
+++ AlisverisSepetim/Data/AlisverisSepetimContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AlisverisSepetim.Models;
+
+namespace AlisverisSepetim.Data
+{
+    public class AlisverisSepetimContext : DbContext
+    {
+        public AlisverisSepetimContext (DbContextOptions<AlisverisSepetimContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Urun> Urunler { get; set; }
+
+        public DbSet<Kategori> Kategoriler { get; set; }
+    }
+}
